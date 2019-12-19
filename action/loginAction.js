5bef43fce0f65e39d41cc1ed68fc3a3c6aec57a9
Chibashini97 <chibashini2014@gmail.com>
2019-12-19 11:57:20 +0530

    2 files modified, 2 added

---
+++ action/loginAction.js
@@ -0,0 +1,7 @@
+const LOGIN_CLICK = 'LOGIN_CLICK'
+
+const loadWelcome = (dispatch) => {
+  return dispatch({type:LOGIN_CLICK})
+}
+
+export default loginAction
---
+++ reducer/loginReducer.js
@@ -0,0 +1,14 @@
+import loginAction from '../../loginAction';
+
+const LoginReducer = (state=login,action) => {
+  switch(action.type){
+    case LOGIN_CLICK : 
+      return 
+      isDoneAuthetication : true;
+    default :
+    return state
+    
+  }
+}
+
+export default loginReducer
